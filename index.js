35bd05c2d87f15c6976bd95090c3f1d1731ddba9
Sujala99 <121869443+Sujala99@users.noreply.github.com>
2025-07-31 00:57:43 +0545

    fixing

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-import Home from "./home";
-import WishList from "./wishlist";
-import ProtectedRoute from "./auth/ProtectedRoute";
-import AdminProtectedRoute from "./auth/AdminProtectedRoute";
-import CartProtectedRoute from "./auth/CartProtectedRoute";
-import { LayoutContext } from "./layout";
-import { layoutState, layoutReducer } from "./layout/layoutContext";
-import { isAdmin, isAuthenticate } from "./auth/fetchApi";
-import PageNotFound from "./layout/PageNotFound";
-import ProductDetails from "./productDetails";
-import ProductByCategory from "./home/ProductByCategory";
-import CheckoutPage from "./order/CheckoutPage";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import {SnackbarProvider} from 'notistack'
+ReactDOM.render(
+  <React.StrictMode>
+<SnackbarProvider maxSnack={3}> 
+    <App />
+    </SnackbarProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-export {
-  Home,
-  WishList,
-  ProtectedRoute,
-  AdminProtectedRoute,
-  CartProtectedRoute,
-  LayoutContext,
-  layoutState,
-  layoutReducer,
-  isAdmin,
-  isAuthenticate,
-  PageNotFound,
-  ProductDetails,
-  ProductByCategory,
-  CheckoutPage,
-};
+serviceWorker.unregister();
